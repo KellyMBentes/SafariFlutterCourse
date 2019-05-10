35e89881df9d894fb2a563d34a9616ecb02f171d
Kelly Bentes <kellymaria.bentes@gmail.com>
2019-05-10 02:22:16 +0200

    Chapter 2 - Passing Data to Stateful Widgets

--- first_app/lib/product_manager.dart
+++ first_app/lib/product_manager.dart
@@ -3,17 +3,38 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct){
+    print("[ProductManager Widget] Constructor");
+  }
+
   @override
   State<StatefulWidget> createState() {
+    print("[ProductManager Widget] createState()");
     return _ProductManagerState();
   }
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ["Food Tester"];
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    print("[ProductManager State] initState()");
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    print("[ProductManager State] didUpdateState()2");
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
+    print("[ProductManager State] build()");
     return Column(children: [
       Container(
         margin: EdgeInsets.all(10.0),
