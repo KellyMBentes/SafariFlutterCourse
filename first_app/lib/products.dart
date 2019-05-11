753a202c17af5b76ca83207276d27459c621bbdf
Kelly Bentes <kellybentes@Barbaras-MacBook-Air.local>
2019-05-11 05:46:37 +0200

    Chapter 5 - Rendering Content Conditionally

--- first_app/lib/products.dart
+++ first_app/lib/products.dart
@@ -3,22 +3,29 @@ import 'package:flutter/material.dart';
 class Products extends StatelessWidget {
   final List<String> products;
 
-  Products ([this.products = const []]){
+  Products([this.products = const []]) {
     print("[Product Widget] Constructor");
   }
-  
+
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(children: <Widget>[
+        Image.asset("assets/food.jpg"),
+        Text(products[index])
+      ]),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     print("[Product Widget] build()");
-    return ListView (
-      children: products
-          .map((element) => Card(
-                child: Column(children: <Widget>[
-                  Image.asset("assets/food.jpg"),
-                  Text(element)
-                ]),
-              ))
-          .toList(),
-    );
+    return products.length > 0
+        ? ListView.builder(
+            itemBuilder: _buildProductItem,
+            itemCount: products.length,
+          )
+        : Center(
+            child: Text("No Products Found Please AddcSome"),
+          );
   }
 }
