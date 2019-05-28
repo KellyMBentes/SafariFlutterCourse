0c51e670305fac800b7dc37ab04ba1476cba90f0
Kelly Bentes <kelly@Joaos-MacBook-Pro.local>
2019-05-28 21:40:23 +0200

    Chapter 6 - Adding Alert Dialogs

--- first_app/lib/pages/product_create.dart
+++ first_app/lib/pages/product_create.dart
@@ -5,7 +5,15 @@ class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Create Products"),
+      child: RaisedButton(
+        child: Text("Save"),
+        onPressed: () {
+          showModalBottomSheet(
+              context: context, builder: (BuildContext context) {
+                return Center(child: Text("This is a Modal"),);
+              });
+        },
+      ),
     );
   }
 }
