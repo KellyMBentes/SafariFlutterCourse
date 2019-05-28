9dafe5fbe4f8426c2aa4d2e6482ccc82b60ad0a3
Kelly Bentes <kelly@Joaos-MacBook-Pro.local>
2019-05-28 13:55:03 +0200

    Chapter6 - Adding the Sidedrawer & the Hamburguer Icon

---
+++ first_app/lib/pages/product_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ProductListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("All Products"),
+    );
+  }
+}
--- first_app/lib/pages/products.dart
+++ first_app/lib/pages/products.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/pages/products_admin.dart';
 import 'package:flutter/material.dart';
 
 import '../product_manager.dart';
@@ -6,10 +7,29 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("EasyList"),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text("Choose"),
+            ),
+            ListTile(
+              title: Text("Manage Products"),
+              onTap: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext build) => ProductAdminPage()));
+              },
+            ),
+          ],
         ),
-        body: ProductManager(),
-      );
+      ),
+      appBar: AppBar(
+        title: Text("EasyList"),
+      ),
+      body: ProductManager(),
+    );
   }
 }
